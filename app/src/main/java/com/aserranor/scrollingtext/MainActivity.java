6206144426093e46f047fd06f9c19080972b424b
Alberto <aserranor@cifpfbmoll.eu>
2021-11-03 02:15:25 +0100

    Second commit. Task 13 finished

--- app/src/main/java/com/aserranor/scrollingtext/MainActivity.java
+++ app/src/main/java/com/aserranor/scrollingtext/MainActivity.java
@@ -2,7 +2,13 @@ package com.aserranor.scrollingtext;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b1 = (Button) findViewById(R.id.addComment);
+        TextView t1 = (TextView) findViewById(R.id.comment);
+        EditText e1 = (EditText) findViewById(R.id.editComment);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (e1.getVisibility() == View.VISIBLE){
+                    e1.setVisibility(View.INVISIBLE);
+                }else {
+                    e1.setVisibility(View.VISIBLE);
+                }
+
+                String content = e1.getText().toString();
+                t1.setText(content);
+            }
+
+        });
     }
 }
